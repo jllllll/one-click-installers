7ad9d0ae95a077800adeb7bb6b710d4a593ce93f
jllllll <jacob201ac@yahoo.com>
2023-03-27 03:31:57 -0500

    Update install-with-powershell.bat

--- install-with-powershell.bat
+++ install-with-powershell.bat
@@ -1,6 +1,3 @@
 cd /D "%~dp0"
-if not exist "%cd%\text-generation-webui-installer.ps1" ( echo text-generation-webui-installer.ps1 not found! && goto end )
+if not exist "%cd%\text-generation-webui-installer.ps1" ( echo text-generation-webui-installer.ps1 not found! && pause && goto eof )
 call powershell.exe -executionpolicy Bypass ". '%cd%\text-generation-webui-installer.ps1'"
-
-:end
-pause
