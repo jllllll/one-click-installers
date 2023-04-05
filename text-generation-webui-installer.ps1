b35ed618128ce2573370b518b544412d2a396b96
jllllll <jacob201ac@yahoo.com>
2023-04-05 16:57:15 -0500

    Specifically target cuda 11.7 ver. of torch 2.0.0
    Move conda-forge channel to global list of channels
    Hopefully prevents missing or incorrect packages

--- text-generation-webui-installer.ps1
+++ text-generation-webui-installer.ps1
@@ -30,8 +30,8 @@ do {
 switch ($gpuChoice)
 {
 	'A' {
-		$packages = 'python=3.10.9','torchvision','torchaudio','pytorch-cuda=11.7','cuda-toolkit','conda-forge::ninja','conda-forge::git'
-		$packageChannels = '-c pytorch','-c nvidia/label/cuda-11.7.0','-c nvidia'
+		$packages = 'python=3.10.9','pytorch[version=2,build=py3.10_cuda11.7*]','torchvision','torchaudio','pytorch-cuda=11.7','cuda-toolkit','ninja','git'
+		$packageChannels = '-c pytorch','-c nvidia/label/cuda-11.7.0','-c nvidia','-c conda-forge'
 	}
 
 	'B' {
